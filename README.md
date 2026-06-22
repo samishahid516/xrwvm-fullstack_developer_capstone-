16ff32eb4fbfb8bc331e96c20d8e9d0148cbb722
Sami Shahid <msamishahid5@gmail.com>
2026-06-22 16:22:55 +0500

    Add capstone task files and placeholders

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+# xrwvm-fullstack_developer_capstone
 
+Project Name: Best Cars Dealership Capstone Project
+Repository Name: xrwvm-fullstack_developer_capstone
---
+++ server/frontend/src/components/Register/Register.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+function Register() {
+  const [userName, setUserName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = () => {
+    // register user
+  };
+
+  return (
+    <div>
+      <h2>Sign-up</h2>
+      <input type="text" placeholder="Username" value={userName} onChange={(e) => setUserName(e.target.value)} />
+      <input type="text" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+      <input type="text" placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
+}
+
+export default Register;
